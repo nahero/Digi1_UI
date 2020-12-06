2becb0f1c4a90fe2a6c750a5ffa36511a1e2c98b
Igor Loncar <igor023@gmail.com>
2020-12-06 19:49:51 +0100

    added tailwind.css, live server, basic config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  // use purge when building for production to remove any unused classes and get the smallest css file size
+  // purge: [
+  //   './src/**/*.html',
+  //   './src/**/*.js',
+  // ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
